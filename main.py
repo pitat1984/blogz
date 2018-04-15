5a18a2a8301251ea6835d1babb5556c8ef7fb47c
Pat McEvoy <pat.mcevoy@hotmail.com>
2018-04-15 16:52:26 -0500

    Got the param part to work and finished build a blog

--- main.py
+++ main.py
@@ -16,24 +16,27 @@ class Blog(db.Model):
 
     def __init__(self, title, blog):
         
+        
         self.title = title
         self.blog = blog
 
-@app.route('/blog', methods=['GET'])
-def blog():
-        id = request.args.get('id')
+@app.route('/blog', methods=['POST', 'GET'])
+def index():
+        
+        
         blogs = Blog.query.all()
-        return render_template('blog.html', title="Build a Blog", id=id,blogs=blogs)
+        return render_template('blog.html', title="Build a Blog",blogs=blogs)
+        
     
    
     
 @app.route('/single-blog', methods=['GET'])
-def single_blog():
+def blog():
     
-    id = int(request.args.get("id"))
+    id = request.args.get("id")
     
     blog = Blog.query.filter_by(id=id).first()
-    return render_template('single-blog.html', title="Build a Blog", id=id, blog=blog)
+    return render_template('single-blog.html', title="Build a Blog",blog=blog)
 
 
 @app.route('/newpost', methods=['POST','GET'])
@@ -53,73 +56,16 @@ def newpost():
             else:
                 blog_error = ''
 
-            return render_template('/newpost.html', title=title, blog=blog,title_error=title_error, blog_error=blog_error)   
+            return render_template('newpost.html', title=title, blog=blog,title_error=title_error, blog_error=blog_error)   
 
         new_blog= Blog(title, blog)
         db.session.add(new_blog)
         db.session.commit()
         blogs = Blog.query.all()
-        return render_template('/blog.html', blogs=blogs)
+        id = str(new_blog.id)
+        return redirect('single-blog?id=' + id)
 
-    return render_template('/newpost.html')
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        user = User.query.filter_by(email=email).first()
-
-        if user and user.password == password:
-            session['email'] = email
-            flash('Logged in')
-            
-            return redirect('/')
-        else:
-            flash('User password incorrect, or user does not exist', 'error')
-            
-
-    return render_template('login.html')
-
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        verify = request.form['verify']
-        existing_user = User.query.filter_by(email=email).first()
-
-        if not existing_user:
-            new_user = User(email,password)
-            db.session.add(new_user)
-            db.session.commit()
-            session['email'] = email
-            return redirect('/')
-        else:
-            return '<h1>Duplicate User!</h1>'
-
-    return render_template('register.html')
-
-
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    
-    owner = User.query.filter_by(email=session['email']).first()
-
-    if request.method == 'POST':
-        task_name = request.form['task']
-        
-        new_task = Task(task_name, owner)
-        db.session.add(new_task)
-        db.session.commit()
-        
-    
-    tasks = Task.query.filter_by(completed=False,owner=owner).all()
-    completed_tasks = Task.query.filter_by(completed=True,owner=owner).all()
-    return render_template('todos.html', title="Get It Done!", tasks=tasks, completed_tasks=completed_tasks)
-
-    return redirect('/')
+    return render_template('newpost.html')
 
 if __name__ == '__main__':
     app.run()
